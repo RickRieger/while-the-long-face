0d81606f3319c4c1b16ac7df3030cee3ede7366a
Rick Rieger <rickrieger36251@gmail.com>
2021-03-05 07:35:04 -0500

    Added another file to hold solutions while practing the solutions

---
+++ alt.js
@@ -0,0 +1,29 @@
+function numberDoubler(num) {
+
+    while(num < 100){
+    num = num * 2;
+    }
+    return num
+}
+
+function stringRepeater(str) {
+    let newStr = ''
+    while(newStr.length < 10){
+        newStr = str + newStr
+    }
+    return newStr
+}
+
+function makeDivisible(x, y) {
+    let outputX = x;
+    while (outputX % y !== 0) {
+        outputX++;
+    }
+    return outputX;
+}
+
+module.exports = {
+    numberDoubler,
+    stringRepeater,
+    makeDivisible
+};
--- main.js
+++ main.js
@@ -1,27 +1,34 @@
 function numberDoubler(num) {
-   
-   
-    while(num < 100){
-        num = num * 2;
-    }
-    return num
-  }
-  
-  function stringRepeater(str) {
-    let newStr = ''
-    while(newStr.length < 10){
-        newStr = str + newStr
-    }
-    return newStr
-  }
-
-  function makeDivisible(x, y) {
-    let outputX = x;
-    while (outputX % y !== 0) {
-        outputX++;
-    }
-    return outputX;
+    
+while(num <= 100){
+  num = num *2
 }
+return num
+}
+
+function stringRepeater(str) {
+let newString = ''
+ while (newString.length < 10){
+     newString += str
+ }
+ return newString
+}
+
+function makeDivisible(x, y) {
+    while(x%y !== 0){
+        x += 1
+    }
+    return x
+
+}
+
+// function makeDivisible(x, y) {
+//     let outputX = x;
+//     while (outputX % y !== 0) {
+//         outputX++;
+//     }
+//     return outputX;
+// }
 
 module.exports = {
     numberDoubler,
